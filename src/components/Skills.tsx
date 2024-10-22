8239d3ae7cc754c13c61d3e98652ac7e524b9299
KartikMouli <kartikmouli0@gmail.com>
2024-10-23 01:27:02 +0530

    ui changes

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -5,7 +5,7 @@ const Skills = () => {
     const techCardsItems = data.currentTech;
 
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 items-center justify-between gap-4 p-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 items-center justify-between gap-4">
             {techCardsItems.length > 0 ? (
                 techCardsItems.map((cardItem) => (
                     <TechCard key={cardItem.name} cardInfo={cardItem} />
