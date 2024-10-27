88b10261a03d0bcc03924aed2e8b1ed7cd5a50d7
KartikMouli <kartikmouli0@gmail.com>
2024-10-27 21:16:55 +0530

    padding fix footer and contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,7 +3,7 @@ import ContactForm from "@/components/ContactForm";
 
 
 const Contact: React.FC = () => (
-    <div className="mt-8 flex flex-col gap-8 pb-16 p-6 rounded-lg shadow-md max-w- mx-auto">
+    <div className="mt-8 flex flex-col px-6 gap-8 pb-16 rounded-lg mx-auto">
         <h1 className="text-3xl font-bold text-gray-100 mb-4">Contact me</h1>
         <p className="text-gray-300 mb-4">I&apos;d love to hear from you! Please fill out the form below.</p>
         <ContactForm />
