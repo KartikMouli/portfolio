7edd85bc81505b708e7c1542739d2592d26c0dbd
KartikMouli <kartikmouli0@gmail.com>
2024-10-28 02:07:17 +0530

    404page changes

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 
 export default function NotFound() {
     return (
-        <article className="flex flex-col gap-8 pb-16 px-4">
-            <div className="min-h-screen flex flex-col items-center justify-center px-4 sm:px-6 sm:py-24">
+        <article className="flex flex-col gap-8 mt-2">
+            <div className="flex flex-col items-center justify-center">
                 <div className="max-w-3xl mx-auto text-center sm:text-left">
                     <section className="flex flex-col sm:flex-row items-center sm:items-start">
                         <p className="text-6xl sm:text-8xl font-bold text-muted-foreground mb-4 sm:mb-0">
@@ -33,32 +33,17 @@ export default function NotFound() {
                                 </object>
                             </div>
                             <p className="mt-2 text-base text-muted-foreground sm:text-lg">
-                                Looks like that page is off on an adventure without us. Don&apos;t worry; it happens to the best of us!
+                                Looks like this page decided to take a little break. Don&apos;t stress; we all need a moment sometimes!
                             </p>
+
                             <p className="mt-4 text-gray-400 text-sm sm:text-base">
-                                Here are a few things you can do:
-                            </p>
-                            <ul className="list-disc pl-5 mt-2 text-gray-300 text-sm sm:text-base">
-                                <li>
-                                    <Link href="/" className="text-blue-500 underline hover:text-blue-300">
-                                        Head back to the homepage
-                                    </Link>
-                                </li>
-                                <li>
-                                    Try searching for what you need in the search bar!
-                                </li>
-                                <li>
-                                    Still lost? Feel free to <Link href="/contact" className="text-blue-500 underline hover:text-blue-300">contact me</Link>.
-                                </li>
-                            </ul>
-                            <p className="mt-4 text-gray-400 text-sm sm:text-base">
-                                Thanks for your patience! I promise the rest of the site is much easier to navigate.
+                                Thanks for your patience! The rest of the site is actually pretty chill, I promise.
                             </p>
                         </div>
                     </section>
                     <div className="mt-8 sm:mt-10 flex justify-center sm:justify-start space-x-3">
                         <Link href="/" className="inline-flex items-center text-blue-500 underline hover:text-blue-300 text-sm sm:text-base">
-                            <ArrowLeftIcon className="w-4 h-4 mr-1" /> Head back to the homepage
+                            <ArrowLeftIcon className="w-4 h-4 mr-1" /> Back to reality, aka the homepage
                         </Link>
                     </div>
                 </div>
