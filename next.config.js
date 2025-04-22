c05f2fdc078fd48a70a1f52fd64b871c71255c4a
KartikMouli <kartikmouli0@gmail.com>
2025-04-22 21:54:19 +0530

    fix:added image domains in next.config.js

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      'leetcard.jacoblin.cool',
+      'raw.githubusercontent.com',
+      'github-readme-streak-stats.herokuapp.com',
+      'holopin.me'
+    ],
+  },
+}
+
+module.exports = nextConfig 
