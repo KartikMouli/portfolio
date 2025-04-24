bda4b7911c7f084096f64c607908115cae1a28e9
KartikMouli <kartikmouli0@gmail.com>
2025-04-25 02:06:35 +0530

    fix:params should be await

--- src/app/api/movies/[id]/videos/route.ts
+++ src/app/api/movies/[id]/videos/route.ts
@@ -1,10 +1,8 @@
 import { NextResponse } from 'next/server';
 import axios from 'axios';
 
-export async function GET(
-    request: Request,
-    { params }: { params: { id: string } }
-) {
+export async function GET(request: Request, props: { params: Promise<{ id: string }> }) {
+    const params = await props.params;
     try {
         // Ensure params are properly awaited
         const { id } = await params;
