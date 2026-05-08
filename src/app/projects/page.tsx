548ba415881bab45f1c6536b5abba5ab38c8322c
monchi <kartikmouli0@gmail.com>
2026-05-08 23:37:24 +0530

    feat(projects): tag filter with zustand on /projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,11 +1,13 @@
-import Projects from '@/components/project';
+import { ProjectsFilterChips } from '@/components/project/filter-chips';
+import { FilteredProjects } from '@/components/project/filtered-projects';
 import { H1 } from '@/components/typography';
 
 export default function ProjectsPage() {
   return (
-    <div className="mt-8 flex flex-col gap-8 pb-16">
+    <div className="mt-8 flex flex-col gap-6 pb-16">
       <H1>My projects</H1>
-      <Projects />
+      <ProjectsFilterChips />
+      <FilteredProjects />
     </div>
   );
 }
